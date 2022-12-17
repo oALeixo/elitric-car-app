2fb05d9078ca8b272244d70d891aeadb5c313e5d
thainan <thainamaleixo@gmail.com>
2022-12-17 17:49:37 -0300

    Adicionado estrutura de tables de carros

--- app/src/main/java/br/com/oaleixo/eleticcarapp/data/local/CarrosContract.kt
+++ app/src/main/java/br/com/oaleixo/eleticcarapp/data/local/CarrosContract.kt
@@ -3,13 +3,25 @@ package br.com.oaleixo.eleticcarapp.data.local
 import android.provider.BaseColumns
 
 object CarrosContract {
-    object CarEntry : BaseColumns
+
+        object CarEntry : BaseColumns {
         const val TABLE_NAME = "car"
-        const val COLUMN_NAME_ID = "id"
         const val COLUMN_NAME_PRECO = "preco"
         const val COLUMN_NAME_BATERIA = "bateria"
         const val COLUMN_NAME_POTENCIA = "potencia"
         const val COLUMN_NAME_RECARGA = "recarga"
         const val COLUMN_NAME_URL_PHOTO = "url_photo"
+    }
 
+    const val TABLE_CAR =
+        "CREATE TABLE ${CarEntry.TABLE_NAME} (" +
+                "${BaseColumns._ID} INTERGER PRIMARY KEY," +
+                "${CarEntry.COLUMN_NAME_PRECO} TEXT," +
+                "${CarEntry.COLUMN_NAME_BATERIA} TEXT," +
+                "${CarEntry.COLUMN_NAME_POTENCIA} TEXT," +
+                "${CarEntry.COLUMN_NAME_RECARGA} TEXT," +
+                "${CarEntry.COLUMN_NAME_URL_PHOTO} TEXT)"
+
+    const val SQL_DELETE_ENTRIES =
+        "DROP TABLE IF EXISTS ${CarEntry.TABLE_NAME}"
 }
