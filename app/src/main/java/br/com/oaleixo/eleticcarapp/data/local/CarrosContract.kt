40a6843358980e5d4b44c181cab19ff5ba1ef8b3
thainan <thainamaleixo@gmail.com>
2022-12-17 17:32:48 -0300

    SQLite iniciando a estruturação

---
+++ app/src/main/java/br/com/oaleixo/eleticcarapp/data/local/CarrosContract.kt
@@ -0,0 +1,15 @@
+package br.com.oaleixo.eleticcarapp.data.local
+
+import android.provider.BaseColumns
+
+object CarrosContract {
+    object CarEntry : BaseColumns
+        const val TABLE_NAME = "car"
+        const val COLUMN_NAME_ID = "id"
+        const val COLUMN_NAME_PRECO = "preco"
+        const val COLUMN_NAME_BATERIA = "bateria"
+        const val COLUMN_NAME_POTENCIA = "potencia"
+        const val COLUMN_NAME_RECARGA = "recarga"
+        const val COLUMN_NAME_URL_PHOTO = "url_photo"
+
+}
